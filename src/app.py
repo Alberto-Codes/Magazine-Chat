e176a36854c8799f359bd159e0a23ff82d201b62
Alberto-Codes <alberto.nieto.80@gmail.com>
2024-03-21 11:59:55 -0700

    feat: enhance API with CORS and OpenAPI documentation
    - Add `flask-cors` to enable Cross-Origin Resource Sharing (CORS) for the Flask app, improving API accessibility.
    - Update API documentation to include server information and a new endpoint for serving the OpenAPI Specification directly.
    - Implement the `/docs` endpoint in the Flask app to serve the OpenAPI Specification document, facilitating API exploration and integration.

--- src/app.py
+++ src/app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+
+from flask import Flask, send_from_directory
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
+
+@app.route("/docs")
+def serve_docs():
+    return send_from_directory("../docs", "API.yml")
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
