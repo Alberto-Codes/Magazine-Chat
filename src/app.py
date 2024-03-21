4f4d0a923ae4664ba50050d9f46d325d584f2684
Alberto-Codes <alberto.nieto.80@gmail.com>
2024-03-21 13:27:00 -0700

    feat: integrate Flask-RESTx for API structure
    - Add `flask-restx` to the project, enabling enhanced API development with automatic Swagger documentation.
    - Initialize Flask-RESTx `Api` object to organize and document API endpoints more efficiently.
    - Create a namespace for magazine-related operations, laying the groundwork for future API expansion.
    - Adjust the serving of OpenAPI documentation to correctly locate the `docs` directory relative to the `app.py` file.

--- src/app.py
+++ src/app.py
@@ -2,9 +2,19 @@ import os
 
 from flask import Flask, send_from_directory
 from flask_cors import CORS
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
 CORS(app)
+
+api = Api(
+    app,
+    version="1.0",
+    title="Magazine Chat API",
+    description="A simple Magazine Chat API",
+)
+
+ns = api.namespace("magazine", description="Magazine Chat API operations")
 
 
 @app.route("/")
@@ -14,7 +24,8 @@ def hello_world():
 
 @app.route("/docs")
 def serve_docs():
-    return send_from_directory("../docs", "API.yml")
+    docs_directory = os.path.join(os.path.dirname(__file__), "../docs")
+    return send_from_directory(docs_directory, "API.yml")
 
 
 if __name__ == "__main__":
