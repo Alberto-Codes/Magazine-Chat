2e177d5adb657bfb19163679c1fe4fd330190668
Alberto Nieto <94092485+Alberto-Codes@users.noreply.github.com>
2024-03-23 17:48:17 -0700

    release/v0.4 (#10)
    * feat: integrate Google OAuth and environmental setup
    
    - Add Flask-Dance for Google OAuth integration in Pipfile and requirements.txt.
    - Include Google Cloud libraries and python-dotenv for environment variable management.
    - Implement OAuth required decorator to protect routes based on environment.
    - Setup route for Google OAuth login and user info retrieval in app.py.
    - Apply OAuth protection to the health check and greetings routes.
    - Remove cross_origin import and usage due to changes in CORS setup.
    
    * refactor: streamline app configuration and enhance OAuth setup
    
    - Refine basic and OAuth-specific configuration in app.py.
    - Remove session and request imports to simplify the OAuth flow.
    - Adjust OAuth scopes for more specific access permissions.
    - Implement environment-sensitive OAuthlib HTTPS requirement bypass for local development.
    - Consolidate API initialization and documentation setup.
    - Add a descriptive comment for the `oauth_required` decorator.
    - Rearrange code for better readability and maintainability.
    
    * feat: enhance deployment and environment configuration
    
    - Introduce dotenv for environment variable management in app.py.
    - Update GitHub Actions workflow to include environment variables and secrets for UAT deployment.
    - Add client secret JSON files to .gitignore for security.
    - Refactor app configuration for clarity, including OAuth setup adjustments.
    - Enable insecure transport for OAuth in local environment for development convenience.
    - Set debug mode based on the environment to facilitate debugging in development.

--- src/app.py
+++ src/app.py
@@ -1,51 +1,97 @@
 import os
-from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
+from functools import wraps
+
+from dotenv import load_dotenv
+from flask import Flask, jsonify, redirect, url_for
+from flask_cors import CORS
+from flask_dance.contrib.google import google, make_google_blueprint
 from flask_restx import Api, Resource
 
+load_dotenv()
+
+
 def create_app(test_config=None):
-    # Instantiate the app
     app = Flask(__name__)
     if test_config:
         app.config.update(test_config)
 
-    # Enable CORS with more granularity
+    app.secret_key = os.getenv("FLASK_SECRET_KEY", "supersecret")
+    environment = os.getenv("ENVIRONMENT", "local")
+
+    if environment == "local":
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    app.config["GOOGLE_OAUTH_CLIENT_ID"] = os.getenv("GOOGLE_OAUTH_CLIENT_ID")
+    app.config["GOOGLE_OAUTH_CLIENT_SECRET"] = os.getenv("GOOGLE_OAUTH_CLIENT_SECRET")
+    google_bp = make_google_blueprint(
+        scope=[
+            "https://www.googleapis.com/auth/userinfo.profile",
+            "openid",
+            "https://www.googleapis.com/auth/userinfo.email",
+        ]
+    )
+    app.register_blueprint(google_bp, url_prefix="/login")
+
+    def oauth_required(f):
+        """Decorator to protect routes with OAuth in non-local environments"""
+
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if environment != "local" and not google.authorized:
+                return redirect(url_for("google.login"))
+            return f(*args, **kwargs)
+
+        return decorated_function
+
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-    # Health check route at the root path
-    @app.route('/')
-    def health_check():
-        """API Health Check"""
-        return jsonify({'status': 'healthy', 'message': 'API operational. Visit /api-docs for docs.'}), 200
-
-    # Initialize API with Flask-RESTX
     api = Api(
         app,
-        version='1.0',
-        title='Content Management API',
-        description='A comprehensive API for content interaction and data processing.',
-        doc='/api-docs',
-        prefix='/api'
+        version="1.0",
+        title="Content Management API",
+        description="A comprehensive API for content interaction and data processing.",
+        doc="/api-docs",
+        prefix="/api",
     )
 
-    # Add namespaces
-    add_namespaces(api)
+    add_namespaces(api, oauth_required)
+
+    @app.route("/")
+    @oauth_required
+    def health_check():
+        return jsonify(
+            {
+                "status": "healthy",
+                "message": "API operational. Visit /api-docs for docs.",
+            }
+        )
+
+    @app.route("/login")
+    def login():
+        if not google.authorized:
+            return redirect(url_for("google.login"))
+        resp = google.get("/oauth2/v1/userinfo")
+        assert resp.ok, resp.text
+        return jsonify({"message": f"Logged in as {resp.json()['email']}."})
 
     return app
 
-def add_namespaces(api):
-    # API v1 namespace
-    api_v1 = api.namespace('v1', description='API Version 1')
 
-    @api_v1.route('/greetings')
+def add_namespaces(api, oauth_required):
+    api_v1 = api.namespace("v1", description="API Version 1")
+
+    @api_v1.route("/greetings")
     class Greetings(Resource):
-        @api.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'}, description='Get a greeting')
+        @api.doc(
+            responses={200: "OK", 400: "Invalid Argument", 500: "Mapping Key Error"},
+            description="Get a greeting",
+        )
+        @oauth_required
         def get(self):
-            """Get a simple greeting"""
-            return {'greeting': 'Hello, world!'}
+            return {"greeting": "Hello, world!"}
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = create_app()
-    port = int(os.environ.get('PORT', 8080))
-    # Turn off debug for production
-    app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
