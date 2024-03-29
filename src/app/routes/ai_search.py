4627564084ba223c72087682e8cfa88a0b1d3aef
Alberto-Codes <94092485+Alberto-Codes@users.noreply.github.com>
2024-03-28 17:19:20 -0700

    feat(ai_search): wrap search functionality in try-except block
    Wrap the search functionality in the `AiSearch` class's `post` method within a try-except block to handle exceptions gracefully. This change aims to improve error handling by returning a structured error response containing the status "Error" and an error message when exceptions occur. This enhancement ensures that the application can provide more informative feedback to the client in case of failures during the search operation.

--- src/app/routes/ai_search.py
+++ src/app/routes/ai_search.py
@@ -11,65 +11,68 @@ from ..config import Config
 
 class AiSearch(Resource):
     def post(self):
-        data = request.get_json(force=True)
-        project_id = Config.GOOGLE_CLOUD_PROJECT
-        location = data.get("location", "global")
-        engine_id = data.get("engine_id")
-        search_query = data.get("search_query")
+        try:
+            data = request.get_json(force=True)
+            project_id = Config.GOOGLE_CLOUD_PROJECT
+            location = data.get("location", "global")
+            engine_id = data.get("engine_id")
+            search_query = data.get("search_query")
 
-        client_options = (
-            ClientOptions(api_endpoint=f"{location}-discoveryengine.googleapis.com")
-            if location != "global"
-            else None
-        )
-        client = discoveryengine.SearchServiceClient(client_options=client_options)
-        serving_config = f"projects/{project_id}/locations/{location}/collections/default_collection/engines/{engine_id}/servingConfigs/default_config"
+            client_options = (
+                ClientOptions(api_endpoint=f"{location}-discoveryengine.googleapis.com")
+                if location != "global"
+                else None
+            )
+            client = discoveryengine.SearchServiceClient(client_options=client_options)
+            serving_config = f"projects/{project_id}/locations/{location}/collections/default_collection/engines/{engine_id}/servingConfigs/default_config"
 
-        content_search_spec = discoveryengine.SearchRequest.ContentSearchSpec(
-            snippet_spec=discoveryengine.SearchRequest.ContentSearchSpec.SnippetSpec(
-                return_snippet=True
-            ),
-            summary_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec(
-                summary_result_count=5,
-                include_citations=True,
-                ignore_adversarial_query=True,
-                ignore_non_summary_seeking_query=True,
-                model_prompt_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelPromptSpec(
-                    preamble="YOUR_CUSTOM_PROMPT"
+            content_search_spec = discoveryengine.SearchRequest.ContentSearchSpec(
+                snippet_spec=discoveryengine.SearchRequest.ContentSearchSpec.SnippetSpec(
+                    return_snippet=True
                 ),
-                model_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelSpec(
-                    version="stable",
-                ),
-            ),
-        )
-
-        ai_request = discoveryengine.SearchRequest(
-            serving_config=serving_config,
-            query=search_query,
-            page_size=10,
-            content_search_spec=content_search_spec,
-            query_expansion_spec=discoveryengine.SearchRequest.QueryExpansionSpec(
-                condition=discoveryengine.SearchRequest.QueryExpansionSpec.Condition.AUTO,
-            ),
-            spell_correction_spec=discoveryengine.SearchRequest.SpellCorrectionSpec(
-                mode=discoveryengine.SearchRequest.SpellCorrectionSpec.Mode.AUTO
-            ),
-        )
-
-        search_results = client.search(ai_request)
-
-        result = {
-            "Answer": search_results.summary.summary_text,
-            "References": [
-                {
-                    "Title": item.document.derived_struct_data["title"],
-                    "Document": item.document.derived_struct_data["link"].replace(
-                        "gs://", "https://storage.cloud.google.com/"
+                summary_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec(
+                    summary_result_count=5,
+                    include_citations=True,
+                    ignore_adversarial_query=True,
+                    ignore_non_summary_seeking_query=True,
+                    model_prompt_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelPromptSpec(
+                        preamble="YOUR_CUSTOM_PROMPT"
                     ),
-                }
-                for item in search_results
-            ],
-            "Status": "Success",
-        }
+                    model_spec=discoveryengine.SearchRequest.ContentSearchSpec.SummarySpec.ModelSpec(
+                        version="stable",
+                    ),
+                ),
+            )
 
-        return result
+            ai_request = discoveryengine.SearchRequest(
+                serving_config=serving_config,
+                query=search_query,
+                page_size=10,
+                content_search_spec=content_search_spec,
+                query_expansion_spec=discoveryengine.SearchRequest.QueryExpansionSpec(
+                    condition=discoveryengine.SearchRequest.QueryExpansionSpec.Condition.AUTO,
+                ),
+                spell_correction_spec=discoveryengine.SearchRequest.SpellCorrectionSpec(
+                    mode=discoveryengine.SearchRequest.SpellCorrectionSpec.Mode.AUTO
+                ),
+            )
+
+            search_results = client.search(ai_request)
+
+            result = {
+                "Answer": search_results.summary.summary_text,
+                "References": [
+                    {
+                        "Title": item.document.derived_struct_data["title"],
+                        "Document": item.document.derived_struct_data["link"].replace(
+                            "gs://", "https://storage.cloud.google.com/"
+                        ),
+                    }
+                    for item in search_results
+                ],
+                "Status": "Success",
+            }
+
+            return result
+        except Exception as e:
+            return {"Status": "Error", "Message": str(e)}
